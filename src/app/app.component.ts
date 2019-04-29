a0ff4525b3238d30790b7a5821ea5e292dbc27a0
ansh-agarwal <spagarwalansh@gmail.com>
2019-04-29 09:45:48 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Sanitizer } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'httpAllFileType';
+  constructor(
+    private http: HttpClient
+    ) {
+// tslint:disable-next-line: max-line-length
+    this.getCss( 'https://ljimageblob.blob.core.windows.net/wayfinder-blob/wayfinder-common/38/webstyles/b95640c0-1522-44bd-ba2c-74dac200b9f6.css?v=1556461993328' );
+    this.getPng('http://3.bp.blogspot.com/-s_pNmSw1ncc/UT5Z-fK-hqI/AAAAAAAAAv0/qCtPX3edwAA/s1600/CirculoPistacho4.png' );
+    this.getSvg('https://ljrestservice.azurewebsites.net/api/Map/GetSvgMap?returnFor=web&filename=1972-theybuilding.svg' );
+  }
+
+// locationMarkerBg
+  getCss(path: string) {
+    return this.http.get(path, {responseType : 'text'})
+    .subscribe((result: any) => {
+        const blob = new Blob([result], {type : 'text/css'});
+        const url = URL.createObjectURL(blob);
+        const fileref: any = document.createElement('link');
+        fileref.setAttribute('rel', 'stylesheet');
+        fileref.setAttribute('href', url);
+        document.getElementsByTagName('head')[0].appendChild(fileref);
+    });
+  }
+
+  getPng(path: string) {
+    return this.http.get(path, {responseType : 'blob'})
+    .subscribe((result: any) => {
+        const blob = new Blob([result], {type : 'application/png'});
+        const url = URL.createObjectURL(blob);
+        const fileref: any = document.createElement('img');
+        fileref.setAttribute('src', url);
+        document.getElementsByTagName('body')[0].appendChild(fileref);
+    });
+  }
+  getSvg(path: string) {
+    return this.http.get(path, {responseType : 'blob'})
+    .subscribe((result: any) => {
+        const blob = new Blob([result], {type : 'image/svg+xml'});
+        const url = URL.createObjectURL(blob);
+        const fileref: any = document.createElement('img');
+        fileref.setAttribute('src', url);
+        document.getElementsByTagName('body')[0].appendChild(fileref);
+    });
+  }
 }
